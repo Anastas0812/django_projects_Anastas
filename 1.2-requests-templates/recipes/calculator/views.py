8f615bc73431cd31ebd65032d4f179ff031db62a
anastasy_klimanova <klimanova_anastasy@mail.ru>
2024-09-23 18:12:19 +0300

    initial comit

--- 1.2-requests-templates/recipes/calculator/views.py
+++ 1.2-requests-templates/recipes/calculator/views.py
@@ -16,15 +16,24 @@ DATA = {
         'сыр, ломтик': 1,
         'помидор, ломтик': 1,
     },
-    # можете добавить свои рецепты ;)
 }
 
-# Напишите ваш обработчик. Используйте DATA как источник данных
-# Результат - render(request, 'calculator/index.html', context)
-# В качестве контекста должен быть передан словарь с рецептом:
-# context = {
-#   'recipe': {
-#     'ингредиент1': количество1,
-#     'ингредиент2': количество2,
-#   }
-# }
+
+def recipe_view(request, recipe_name):
+    # Получаем список ингредиентов для указанного блюда
+    ingredients = DATA.get(recipe_name)
+    #Получаем кол-во человек для приготовления блюда (1 = по умолчанию)
+    servings = request.GET.get('servings', 1)
+    if ingredients is None:
+        # Если рецепт не найден, возвращаем пустой контекст
+        context = {
+            'recipe': {}, #благодаря шаблону html браузер отдаст "Такого рецепта не знаю :("
+            'recipe_name': recipe_name  # Передаем название блюда в контекст
+        }
+    else:
+        context = {
+            'recipe': {ingredient: float(quantity) * int(servings) for ingredient, quantity in ingredients.items()},
+            'recipe_name': recipe_name  # Передаем название блюда в контекст
+        }
+    return render(request, 'calculator/index.html', context)
+
